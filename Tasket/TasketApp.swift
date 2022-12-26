134606e3dd05ffc404999efa35bc9d8f7040c005
Bugra Muaz Mujde <mujdebugra@gmail.com>
2022-12-26 21:01:24 +0300

    Initial Commit

---
+++ Tasket/TasketApp.swift
@@ -0,0 +1,20 @@
+//
+//  TasketApp.swift
+//  Tasket
+//
+//  Created by queque on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TasketApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
